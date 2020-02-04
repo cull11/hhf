d0aadb4c31cf5ed846790954e6afc43090564e9c
cull11 <60672194+cull11@users.noreply.github.com>
2020-02-05 00:41:33 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("671460740191748138")
+setInterval(function() {
+channel.send(`اyousif112 yousif112 yousif112 yousif112`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
